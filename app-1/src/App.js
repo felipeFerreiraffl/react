5bf525d1a3498ce64b4945373e1c7772b6939fd6
Felipe Ferreira <felipado2x0@gmail.com>
2024-04-28 16:07:09 -0300

    docs: changed view on website

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -5,20 +5,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Teste de React
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          React-App
-        </a>
-        <h1>Flamengo</h1>
-        <img src="https://a.espncdn.com/combiner/i?img=/i/teamlogos/soccer/500/819.png" className="App-logo" alt="flamengo" />
+      <h1>Flamengo</h1>
+        <img 
+        src="https://a.espncdn.com/combiner/i?img=/i/teamlogos/soccer/500/819.png"
+         className="Flamengo" 
+         alt="flamengo" 
+         />
       </header>
     </div>
   );
