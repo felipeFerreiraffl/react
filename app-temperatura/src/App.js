706cb74907be68497eeed61639df2a7833825eff
Felipe Ferreira <felipado2x0@gmail.com>
2024-05-30 16:55:25 -0300

    feat: created the web app and add a style that changes the color of the card based on temperature

--- app-temperatura/src/App.js
+++ app-temperatura/src/App.js
@@ -1,17 +1,25 @@
 import './App.css';
-import EstadoCard from './components/estado/estado';
-import Resposta from './components/resposta/resposta';
-import PrevisaoCard from './components/previsao/previsao';
+import { EstadoCard } from "./components/estado/index";
+import { PrevisaoCard } from './components/previsao';
+import { Resposta } from "./components/resposta/index";
+import { useState } from 'react';
 
 function App() {
+  const [dados, setDados] = useState({
+    nome: '...',
+    temp: '0°C',
+    desc: '...'
+  });
+
+  const [previsao, setPrevisao] = useState([]);
 
   return (
     <div className="App">       
       <div className='card'>
-        <EstadoCard></EstadoCard>
-        <Resposta></Resposta>
+        <EstadoCard setDados={setDados} setPrevisao={setPrevisao} />
+        <Resposta dados={dados} />
       </div>
-      <PrevisaoCard></PrevisaoCard>
+      <PrevisaoCard dados={previsao} />
     </div>
   );
 }
